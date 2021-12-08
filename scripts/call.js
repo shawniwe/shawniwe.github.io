70a9af2b6a80fd3ce8fac6fc6e4b0bd78a1d78c3
Shawniwe <42480396+shawniwe@users.noreply.github.com>
2021-12-08 18:04:55 +0300

    Add files via upload

--- scripts/call.js
+++ scripts/call.js
@@ -1,7 +1,25 @@
 let callBtn = document.querySelector(".header__menu-phonecall a");
 let number = document.querySelector(".phone-number");
+let numbersBlock = document.querySelector(".header__menu-picknumber");
+let numbers = numbersBlock.querySelectorAll(".header__menu-picknumber-item");
+let closeButton = document.querySelector(".header__menu-picknumber .button-orange");
 
-number.onchange = function() {
-    console.log(number.value);
-    callBtn.href = "tel:" + number.value;
+console.log(numbersBlock);
+console.log(numbers);
+
+number.onclick = function() {
+    numbersBlock.classList.toggle("header__menu-picknumber--hidden");
+}
+
+numbers.forEach(x => x.onclick = function() {
+    callBtn.href = "tel:" + x.innerText;
+    number.innerText = x.innerText;
+
+    numbers.forEach(x => x.classList.remove("header__menu-picknumber-item--selected"));
+
+    x.classList.add("header__menu-picknumber-item--selected");
+});
+
+closeButton.onclick = function() {
+    numbersBlock.classList.toggle("header__menu-picknumber--hidden");
 }
