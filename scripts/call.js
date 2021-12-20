684998bcf637828bb267852491530a66a0c6d9ac
Shawniwe <42480396+shawniwe@users.noreply.github.com>
2021-12-20 17:39:56 +0300

    Add files via upload

--- scripts/call.js
+++ scripts/call.js
@@ -7,19 +7,43 @@ let closeButton = document.querySelector(".header__menu-picknumber .button-orang
 console.log(numbersBlock);
 console.log(numbers);
 
-number.onclick = function() {
-    numbersBlock.classList.toggle("header__menu-picknumber--hidden");
+let isVisible = true;
+let isCursorOnNumbers = false;
+
+number.onmouseenter = function() {
+    numbersBlock.classList.remove("header__menu-picknumber--hidden");
+    isVisible = true;
+}
+
+number.onmouseleave = function () {
+    setTimeout(() => {
+        if(!isCursorOnNumbers && isVisible) {
+            numbersBlock.classList.add("header__menu-picknumber--hidden");
+            isVisible = false;
+    
+            console.log('visible: ' + isVisible);
+            console.log('on numbers: ' + isCursorOnNumbers);
+        }
+    }, 100);
+}
+
+numbersBlock.onmouseenter = function () {
+    isCursorOnNumbers = true;
+}
+
+numbersBlock.onmouseleave = function() {
+    if(isVisible && isCursorOnNumbers) {
+        numbersBlock.classList.add("header__menu-picknumber--hidden");
+        isVisible = false;
+        isCursorOnNumbers = false;
+    }
 }
 
 numbers.forEach(x => x.onclick = function() {
     callBtn.href = "tel:" + x.innerText;
-    number.innerText = x.innerText;
+    number.innerText = x.querySelector(".header__menu-picknumber-item-number").innerText;
 
     numbers.forEach(x => x.classList.remove("header__menu-picknumber-item--selected"));
 
     x.classList.add("header__menu-picknumber-item--selected");
 });
-
-closeButton.onclick = function() {
-    numbersBlock.classList.toggle("header__menu-picknumber--hidden");
-}
